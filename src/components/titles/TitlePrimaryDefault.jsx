43f050c3ac74ae3a0204f26024127cabc013fb4a
imgVOID <imgvoid@gmail.com>
2025-07-17 20:32:21 +0000

    Change font props

--- src/components/titles/TitlePrimaryDefault.jsx
+++ src/components/titles/TitlePrimaryDefault.jsx
@@ -4,7 +4,7 @@ function TitlePrimaryDefault({title="", desc=""}) {
       <h2 className="font-primary-1 text-5xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-base-content to-accent leading-relaxed">
         { title }
       </h2>
-      <p className="font-primary-2 font-normal text-xl text-base-content/75 max-w-2xl mx-auto">
+      <p className="font-primary-2 text-xl text-base-content/75 max-w-2xl mx-auto">
         { desc }
       </p>
     </div>
