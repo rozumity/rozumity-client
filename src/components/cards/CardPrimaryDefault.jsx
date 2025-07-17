39705046dc1f546c9a3221f7ebeb13d968989207
imgVOID <imgvoid@gmail.com>
2025-07-17 22:03:28 +0000

    Change titles and cards styles

--- src/components/cards/CardPrimaryDefault.jsx
+++ src/components/cards/CardPrimaryDefault.jsx
@@ -1,5 +1,5 @@
 function CardPrimaryDefault({feature, index}) {
-    const cardStyle = `group relative bg-gradient-to-t from-secondary/35 to-base-50 rounded-2xl p-8 hover:shadow-2xl transform hover:-translate-y-2 transition-all duration-300 shadow-3xl shadow-accent-content/20`
+    const cardStyle = `group relative bg-gradient-to-t from-secondary/35 to-base-50 rounded-2xl p-8 hover:shadow-2xl transform hover:-translate-y-2 transition-all duration-300 shadow-2xl shadow-accent-content/25`
     return (
         <div key={index} className={ cardStyle }>
             <div className="absolute inset-0 rounded-b-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
@@ -7,10 +7,10 @@ function CardPrimaryDefault({feature, index}) {
             <div className="text-6xl mb-6 text-center group-hover:scale-110 transition-transform duration-300 text-shadow-lg">
                 {feature.icon}
             </div>
-            <h3 className="text-xl uppercase text-center font-bold font-secondary-1 mb-4 text-accent text-shadow-sm text-shadow-accent-content/10 group-hover:text-info transition-colors">
+            <h3 className="text-xl uppercase text-center font-bold font-secondary-1 mb-4 text-accent text-shadow-md text-shadow-accent-content/10 group-hover:text-info transition-colors">
                 {feature.title}
             </h3>
-            <p className="font-secondary-2 text-center leading-relaxed text-shadow-sm text-shadow-accent-content/15">
+            <p className="font-secondary-2 text-center leading-relaxed text-shadow-sm text-shadow-accent-content/10">
                 {feature.description}
             </p>
             </div>
--- src/components/titles/TitlePrimaryDefault.jsx
+++ src/components/titles/TitlePrimaryDefault.jsx
@@ -4,7 +4,7 @@ function TitlePrimaryDefault({title="", desc=""}) {
       <h2 className="font-primary-1 text-5xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-base-content to-accent leading-relaxed">
         { title }
       </h2>
-      <p className="font-primary-2 text-xl text-base-content/75 max-w-2xl mx-auto text-shadow-sm text-shadow-accent-content/15">
+      <p className="font-primary-2 text-xl text-base-content/75 max-w-2xl mx-auto text-shadow-sm text-shadow-accent/10">
         { desc }
       </p>
     </div>
